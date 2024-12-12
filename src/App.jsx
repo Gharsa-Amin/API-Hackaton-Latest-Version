5f9d4a682945fbd8a4a63d7a7a0f1ff52369f692
Gharsa Amin <ghars0718@gmail.com>
2024-12-12 09:04:29 -0500

    made first get request from the API endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,15 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CoinList from "./pages/CoinListPages";
 
-export default function CoinList() {
-	const [coins, setCoins] = useState([]);
-
-	useEffect(() => {
-		const fetchCoins = async () => {
-			const URL =
-				"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd";
-			try {
-				const response = await axios.get(URL);
-				console.log(response.data);
-
-				setCoins(response.data);
-			} catch (error) {
-				console.error("Error fetching coins:", error);
-			}
-		};
-		fetchCoins();
-	}, []);
-
+function App() {
 	return (
-		<section className="coin-list">
-			<h1>Cryptocurrency List</h1>
-			{coins.length > 0 ? (
-				<ul>
-					{coins.map((coin) => (
-						<li key={coin.id}>
-							<div>
-								<h2>{coin.name}</h2>
-								<p>{coin.symbol.toUpperCase()}</p>
-								<p>Price: ${coin.current_price}</p>
-								<p>Market Cap: ${coin.market_cap}</p>
-								<p>24h Change: {coin.price_change_percentage_24h}%</p>
-								<img src={coin.image} alt={coin.name} width={50} height={50} />
-							</div>
-						</li>
-					))}
-				</ul>
-			) : (
-				<p>Loading coins...</p>
-			)}
-		</section>
+		<Router>
+			<Routes>
+				<Route path="/" element={<CoinList />} />
+			</Routes>
+		</Router>
 	);
 }
+
+export default App;
